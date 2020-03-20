9fc0935de2869ff7a0061e42a77a5ca2fbe72521
bftawfik <bftawfik@gmail.com>
2020-03-20 23:05:49 +0200

    add the user provider

---
+++ src/Context/UserProvider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const { Provider, Consumer } = React.createContext();
+
+const withUser = Component => {
+  return () => <Consumer>{user => <Component user={user} />}</Consumer>;
+};
+const UserProvider = ({ value, children }) => {
+  return <Provider value={value}>{children}</Provider>;
+};
+export default UserProvider;
+export { UserProvider, withUser };
