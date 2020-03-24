bf021536315524d19596a5df79577dd8781126a0
bftawfik <bftawfik@gmail.com>
2020-03-24 23:31:47 +0200

    update user route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    minlength: 5
+  },
+  username: {
+    type: String,
+    required: true,
     trim: true,
     minlength: 3
   },
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,97 @@
 const router = require('express').Router();
-let User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+const auth = require('../middleware/auth');
+const User = require('../models/user.model');
+
 
 router.route('/add').post((req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const newUser = new User({ username, password});
+  const { email, username, password } = req.body;
+  if (!email || !username || !password) {
+    res.status(400).json({msg: 'Please enter all fields'});
+  }
 
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+  User.findOne({email})
+  .then(user => {
+    if(user){
+      res.status(400).json({ msg: 'User already exists' });
+    }
+
+    const newUser = new User({ email, username, password });
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err) throw err;
+        newUser.password = hash;
+        newUser.save()
+          .then(user => {
+            jwt.sign(
+              { id: user.id },
+              process.env.JWT_SECRET,
+              { expiresIn: 3600 },
+              (err, token) => {
+                if(err) throw err;
+                res.json({
+                  msg: 'User added Successfully',
+                  token,
+                  user: {
+                    id: user.id,
+                    email: user.email,
+                    username: user.username
+                  }
+                }) 
+              })
+              }
+            )
+          .catch(err => res.status(400).json('Error: ' + err))
+      })
+    })
+
+  })
+})
+
+router.route('/login').post((req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        res.status(400).json({ msg: 'User does not exist' });
+      }
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) res.status(400).json({ msg: 'Invalid credentials' });
+          jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: 3600 }, 
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                msg: 'User logged in successfully',
+                token,
+                user: {
+                  id: user.id,
+                  email: user.email,
+                  username: user.username
+                }
+              })
+            })
+        })
+    })
+})
+
+router.get('/auth', auth, (req, res) => {
+  
+  User.findById(req.user.id)
+   .select('-password')
+   .then(user => {
+      const { _id: id, email, username } = user;
+      res.json({ id, email, username });
+    });
+})
 
 module.exports = router;
